43dbec6194a6da7ea0ff58f0a222c96247c5c6d8
Daniel Keyes <danieljkeyes@gmail.com>
2016-01-05 05:27:39 +0100

    Initial commit of src/ directory
    To download the repository, I would clone this repository to some folder
    git clone <repo name> temp
    and then move the contents of temp/ into the src/ directory.

---
+++ sandbox/RobotPlayer.java
@@ -0,0 +1,14 @@
+package sandbox;
+
+import battlecode.common.*;
+
+import java.util.Random;
+
+public class RobotPlayer {
+
+	public static void run(RobotController rc) {
+		while (true) {
+			Clock.yield();
+		}
+	}
+}
